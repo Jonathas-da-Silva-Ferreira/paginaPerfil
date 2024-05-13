de681377066cbd22ecb3f7f986097e88e5300828
Jonathas da Silva Ferreira <105074747+Jonathas-da-Silva-Ferreira@users.noreply.github.com>
2024-05-13 11:25:04 -0300

    termino

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const sidebarItems = document.querySelectorAll('.sidebar .item');
 
 const tableRows = document.querySelectorAll('.main table tbody tr');
 
+const menuBtn = document.getElementById('menu-btn');
+
+const leftSection  = document.querySelector('.left-section');
+
+let isMenuOpen = false;
+
 sidebarItems.forEach(sideItem =>{
     sideItem.addEventListener('click', () =>{
         sidebarItems.forEach(item =>{
@@ -18,4 +24,13 @@ tableRows.forEach(tableTR =>{
         });
         tableTR.classList.add('selected');
     });
+});
+
+menuBtn.addEventListener('click', () =>{
+    if(!isMenuOpen){
+        leftSection.style.left = '0';
+   }else{
+    leftSection.style.left = '-160px';
+   }
+   isMenuOpen = !isMenuOpen;
 });
